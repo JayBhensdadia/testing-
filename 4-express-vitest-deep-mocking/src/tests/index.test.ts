35639742093bd312c5f9844eac2e855478c6b7a6
Jay Bhensdadia <jayvpatel29@gmail.com>
2024-06-12 22:05:49 +0530

    learn: deep-mocking, spy

---
+++ 4-express-vitest-deep-mocking/src/tests/index.test.ts
@@ -0,0 +1,74 @@
+import { describe, it, expect, vi } from 'vitest';
+import request from 'supertest';
+import { app } from '../app';
+import { prismaClient } from '../__mocks__/db';
+
+
+//mock the prisma client
+vi.mock('../db');
+
+describe('POST /sum', () => {
+    it('the sum of 1 and 2 should be 3', async () => {
+
+        prismaClient.sum.create.mockResolvedValue({
+            id: 1,
+            a: 1,
+            b: 2,
+            result: 3
+        });
+
+        vi.spyOn(prismaClient.sum, 'create');
+
+        const res = await request(app).post('/sum').send({
+            a: 1,
+            b: 2
+        });
+
+        expect(prismaClient.sum.create).toHaveBeenCalledWith({
+            data: {
+                a: 1,
+                b: 2,
+                result: 3
+            }
+        });
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(3);
+    });
+
+
+    it('should throw error on incorrect inputs', async () => {
+        const res = await request(app).post('/sum').send({
+            a: 1,
+            b: 'djafldfjjl'
+        });
+
+        expect(res.statusCode).toBe(411);
+        expect(res.body.message).toBe('Incorrect inputs!');
+    });
+});
+
+
+
+describe('GET /sum', () => {
+    it('the sum of 1 and 2 should be 3', async () => {
+        const res = await request(app).get('/sum').set({
+            a: "1",
+            b: "2"
+        }).send();
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(3);
+    });
+
+
+    it('should throw error on incorrect inputs', async () => {
+        const res = await request(app).get('/sum').set({
+            a: "1",
+            b: 'djafldfjjl'
+        }).send();
+
+        expect(res.statusCode).toBe(411);
+        expect(res.body.message).toBe('Incorrect inputs!');
+    });
+});
